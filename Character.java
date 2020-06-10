34e54621435b16d12b64e18c902f14ea21ad8621
repl.it user <replituser@example.com>
2020-06-10 23:04:16 +0000

    [fix] comments

--- Character.java
+++ Character.java
@@ -1,5 +1,7 @@
 /* Character.java: (演習9-2)登場人物クラス
  * Interface:
    IPrintable:(継承)会話文を出力
+ * Parent:
+   Person
  * Childs:
    BattleCharacter: 戦闘可能な登場人物 */
--- Speaker.java
+++ Speaker.java
@@ -1,7 +1,5 @@
 /* Speaker.java: (演習9-1)出力可能な抽象クラス
  * Interface:
    IPrintble: 会話文を出力
- * Overrides:
-   prefix: print内での表示名
  * Childs:
    Person: 人間 */
