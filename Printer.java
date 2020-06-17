2393352f908b6b5a81807796cb286501d6ea5ffe
yasue <yasue@tanokiva.com>
2020-06-17 20:36:32 +0900

    [add] Printer.java: _txtw
    textWindowに表示するように変更

--- Printer.java
+++ Printer.java
@@ -1,10 +1,20 @@
 /* Printer.java: (演習9-1)出力クラス
 IPrintableで用いる出力.最初はSystem.out.printlnを用いる. */
 public class Printer {
-  static public void print(Object message) {
-    System.out.print(message);
-  }
-  static public void println(Object message) {
-    System.out.println(message);
-  }
+	static animation.TextWindow _txtw = null;
+	
+	static public void print(Object message) {
+		if (_txtw == null) {		  
+		    System.out.print(message);
+		} else {
+			_txtw.print_(message);
+		}
+	}
+	static public void println(Object message) {
+		if (_txtw == null) {
+			System.out.println(message);
+		} else {
+			_txtw.println(message);
+		}
+	}
 }
