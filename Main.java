ace026f7d1348f641843dfba55ae2f5515f9baa8
YasueShuta <shuta.59074.orionsbelt@gmail.com>
2020-06-18 00:08:47 +0000

    [delete] Main.test.

--- Main.java
+++ Main.java
@@ -3,27 +3,7 @@ import java.util.*;
 //import animation.*;
 
 class Main {
-  static void test() {
-    ArrayList<IBattlable> b = new ArrayList<>();
-
-    BattleCharacter hero = new BattleCharacter("勇者", 15, "Hero", 1, 100, 100);
-    BattleCharacter satan = new BattleCharacter("魔王", 300, "Satan", 100, 300, 5000);
-    b.add(hero);
-    b.add(satan);
-
-    BattleCharacter tmp = (BattleCharacter)b.get(0);
-    tmp.levelup(49);
-
-    for (IBattlable i : b) {
-      Character c = (Character)i;
-      Printer.println(c.name()+"のレベルは"+c.level());
-    }
-
-    hero.attack(satan);
-    satan.attack(hero, 300);
-  }
   public static void main(String[] args) {
-    //test();
     /* enshu10-1: TODO */
     //DemoWindow.main(args);
   }
