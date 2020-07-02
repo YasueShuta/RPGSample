06dc1937ec9966e981ab2e3e7b5cf6e7a23a0ea3
YasueShuta <shuta.59074.orionsbelt@gmail.com>
2020-07-02 02:07:45 +0000

    [back] enshu12 begin

--- Main.java
+++ Main.java
@@ -3,24 +3,30 @@ import animation.*;
 
 class Main {
     public static void main(String[] args) {
-        Person player = new Person("AAAA", 20);
-
         DemoGacha app = new DemoGacha("Demo") {
             @Override
             public void initHeroes() {
-                IMovable m;
-                
-                Hero hime = new Hero("Hime", 19, 1, 100, 0,
-                    CharaImageFile.FIGHTER_FEMALEe, // srcname
-                    ShadowImageFile.DEFAULT);       // shadowname
-                hime.home(w/2, h/2);
-                setGachaHero(hime);
+                MoveHandle m;
 
-                Wizard taro = new Wizard("Taro", 18, 1, 100, 150,
-                    CharaImageFile.WIZARD_MALE,     // srcname
-                    ShadowImageFile.DEFAULT);       // shadowname
-                taro.home(w/2, h/2);
-                setGachaHero(taro);
+                m = new MoveHandle(
+                    CharaImageFile.getFilename(CharaImageFile.FIGHTER_FEMALEe),
+                    ShadowImageFile.getFilename(ShadowImageFile.DEFAULT),
+                    w/2, h/2);
+                // Hero hime = new Hero("Hime", 19, 1, 100, 0,
+                //     CharaImageFile.FIGHTER_FEMALEe, // srcname
+                //     ShadowImageFile.DEFAULT);       // shadowname
+                //hime.home(w/2, h/2);
+                setGachaHero(m);
+
+                m = new MoveHandle(
+                    CharaImageFile.getFilename(CharaImageFile.WIZARD_MALE),
+                    ShadowImageFile.getFilename(ShadowImageFile.DEFAULT),
+                    w/2, h/2);
+                // Wizard taro = new Wizard("Taro", 18, 1, 100, 150,
+                //     CharaImageFile.WIZARD_MALE,     // srcname
+                //     ShadowImageFile.DEFAULT);       // shadowname
+                //taro.home(w/2, h/2);
+                setGachaHero(m);
 
                 m = new MoveHandle(
                     CharaImageFile.getFilename(CharaImageFile.CATb),
@@ -28,22 +34,9 @@ class Main {
                     w/2, h/2);
                 setGachaHero(m);
 
-                taro.hello_to(hime);
-            }
-
-            @Override
-            public void choice() {
-                super.choice();
-                Printer.clear();
-                Printer.println("Success!");
-                if (cast instanceof Person) {
-                    ((Person)cast).hello(player);
-                }
+                // taro.hello_to(hime);
             }
         };
-        Printer.setTextWindow(app.textWindow);
-        Printer.clear();
-        Printer.println("Hello!\nLet's Gacha!");
         app.run();        
     }
 }
