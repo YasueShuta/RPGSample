ba5f41c10448a55c7f69a0a36ff3c4fbe87e8e15
YasueShuta <shuta.59074.orionsbelt@gmail.com>
2020-07-15 20:32:42 +0000

    [update] add MyApp.java

--- Main.java
+++ Main.java
@@ -3,47 +3,6 @@ import animation.*;
 
 class Main {
     public static void main(String[] args) {
-        Person player = new Person("AAAA", 20);
-
-        DemoGacha app = new DemoGacha("Demo") {
-            @Override
-            public void initHeroes() {
-                IMovable m;
-                
-                Hero hime = new Hero("Hime", 19, 1, 100, 0,
-                    CharaImageFile.FIGHTER_FEMALEe, // srcname
-                    ShadowImageFile.DEFAULT);       // shadowname
-                hime.home(w/2, h/2);
-                setGachaHero(hime);
-
-                Wizard taro = new Wizard("Taro", 18, 1, 100, 150,
-                    CharaImageFile.WIZARD_MALE,     // srcname
-                    ShadowImageFile.DEFAULT);       // shadowname
-                taro.home(w/2, h/2);
-                setGachaHero(taro);
-
-                m = new MoveHandle(
-                    CharaImageFile.getFilename(CharaImageFile.CATb),
-                    ShadowImageFile.getFilename(ShadowImageFile.ANIMAL),
-                    w/2, h/2);
-                setGachaHero(m);
-
-                taro.hello_to(hime);
-            }
-
-            @Override
-            public void choice() {
-                super.choice();
-                Printer.clear();
-                Printer.println("Success!");
-                if (cast instanceof Person) {
-                    ((Person)cast).hello(player);
-                }
-            }
-        };
-        Printer.setTextWindow(app.textWindow);
-        Printer.clear();
-        Printer.println("Hello!\nLet's Gacha!");
-        app.run();        
+        MyApp.main(args);        
     }
 }
---
+++ MyApp.java
@@ -0,0 +1,53 @@
+import animation.*;
+
+public class MyApp extends DemoGacha {
+    Person player = new Person("AAAA", 20);
+
+    public MyApp() {
+        super("My Gacha");
+    }
+
+    @Override
+    public void initHeroes() {
+        IMovable m;
+        
+        Hero hime = Hero.create("Hime", 19, 1, 100, 0,
+            /*is_male*/false, /*variation*/"e");
+        hime.home(w/2, h/2);
+        setGachaHero(hime);
+
+        Wizard taro = Wizard.create("Taro", 18, 1, 100, 150,
+            /*is_male*/true, /*variation*/"");
+        taro.home(w/2, h/2);
+        setGachaHero(taro);
+
+        m = new MoveHandle(
+            CharaImageFile.getFilename(CharaImageFile.CATb),
+            ShadowImageFile.getFilename(ShadowImageFile.ANIMAL),
+            w/2, h/2);
+        setGachaHero(m);
+    }
+
+    @Override
+    public void choice() {
+        super.choice();
+        Printer.clear();
+        Printer.println("Success!");
+        if (cast instanceof Person) {
+            ((Person)cast).hello(player);
+        }
+    }
+
+    public void run() {
+        Printer.setTextWindow(textWindow);
+        Printer.clear();
+        Printer.println("Hello!\nLet's Gacha!");
+
+        super.run();
+    }
+
+    public static void main(String[] args) {
+        MyApp app = new MyApp();
+        app.run();        
+    }
+}
